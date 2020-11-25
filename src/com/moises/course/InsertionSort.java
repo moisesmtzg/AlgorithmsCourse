1818fb49b6721c266ba12f592685205d3be01d9e
moisesmtzg <moisesmtzg@gmail.com>
2020-11-25 15:49:49 -0600

    other commit

--- src/com/moises/course/InsertionSort.java
+++ src/com/moises/course/InsertionSort.java
@@ -2,9 +2,8 @@ package com.moises.course;
 
 public class InsertionSort {
 
-	private int firstUnsortedIndex = 1;
 	public void sort(int[] arr) {
-		for(;firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+		for(int firstUnsortedIndex = 1 ;firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
 			int newElement = arr[firstUnsortedIndex];
 			int i;
 			for(i = firstUnsortedIndex; i > 0 && arr[i-1] > newElement; i--) {
